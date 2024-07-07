617e43ceca0f76ad3064bdd28d657427f43039d7
zyasyafaat <34265613+zyasyafaat@users.noreply.github.com>
2024-07-07 21:22:28 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,14 +7,14 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your worker's environment variable if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'me@lyc8503.net',
+  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'zyasyafaat@hotmail.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "lyc8503's OneDrive",
+  title: "Zya Syafaat Hotmail",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: process.env.BASE_DIRECTORY || '/',
